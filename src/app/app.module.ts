5b45b07cbc147aa83b0dd418acc6a2dd646f9ef3
Milad Raeisi <miladsoft@yahoo.com>
2023-11-30 05:23:19 +0400

    Update Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,19 +6,24 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { I18nModule } from './i18n/i18n.module';
 import { SelectLanguageComponent } from './select-language/select-language.component';
-
+import { ImageSliderModule } from './imageSlider/imageSlider.module';
+ 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SelectLanguageComponent
+    SelectLanguageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    I18nModule
+    I18nModule,
+    ImageSliderModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { SlideInterface } from '../imageSlider/types/slide.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  
   title = 'Jalal Mashhadi Fard';
 
+  slides: SlideInterface[] = [
+    { url: '/assets/images/main/slider/1.jpg', title: '' },
+    { url: '/assets/images/main/slider/2.jpg', title: '' },
+  ];
 }
