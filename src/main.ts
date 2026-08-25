e496fcc3da6758bbd0d49d81a520ecb01781b28c
jalalmashhadifard <152965920+jalalmashhadifard@users.noreply.github.com>
2026-08-25 19:01:03 +0330

    Add New Generation apartment and fix stale portfolio assets

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations()
+    .then(registrations => registrations.forEach(registration => registration.unregister()))
+    .catch(() => undefined);
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
